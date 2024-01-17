964382721e444c858935427884c27f9ff075e5aa
Zeynal <zeynal.mustafayev.099@gmail.com>
2024-01-17 01:50:03 -0800

    start second first

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,27 +1,29 @@
 #include "sort.h"
-
+/**
+ * bubble_sort - sorts an array of integers
+ *
+ * @array: array to sort
+ *
+ * @size: size of array
+ *
+ * Return: array
+ */
 void bubble_sort(int *array, size_t size)
 {
-	size_t first, second, i;
+	size_t first, second;
 	int temp;
 
 	for (first = 0; first < size - 1; first++)
 	{
 		for (second = 0; second < size - first - 1; second++)
 		{
-			if (second < first)
+			if (array[second] > array[second + 1])
 			{
-				temp = array[first];
-				array[first] = array[second];
-				array[second] = temp;
+				temp = array[second];
+				array[second] = array[second + 1];
+				array[second + 1] = temp;
+				print_array(array, size);
 			}
-				
 		}
-		//printf("%lu ", first + 1);
-		for (i = 0; i < size; i++)
-		{
-			printf("%d ", array[i]);
-		}
-		printf("\n");
 	}
 }
